68b17f79ab1d8debdecb03566b72aefff3ee8e56
AltanKaracan <altanmk93@gmail.com>
2019-06-10 18:30:13 +0200

    first version

--- base.import.py
+++ base.import.py
@@ -7,14 +7,32 @@ import requests
 import re
 import wikidataintegrator as WI
 import sys
+import threading
+import time
 from pathlib import Path
 
-
+max_threads = 30
 # overwrite the wait function
 def wait(self, secs):
     pass
 
+def threader(thread_list):
+	for a in thread_list:
+		while len([True for b in thread_list if b.is_alive()]) >= max_threads:
+			time.sleep(0.1)
+		a.start()
+	while not len([None for a in thread_list if a.is_alive()]) == 0:
+		time.sleep(0.1)
 
+prop_map = {}
+props = pd.read_csv('./data/saved_properties.csv')
+for _, row in props.iterrows():
+    prop_map[row['id']]={
+       'PID': row["PID"],
+       'datatype': row['data_type']}
+
+
+"""
 pywikibot.throttle.Throttle.wait = wait
 site = pywikibot.Site('wikidata', 'wikidata')
 site.login()
@@ -22,8 +40,8 @@ site.login()
 # import properties
 
 prop_map = {}
-
 props = pd.read_csv('./data/properties.csv')
+
 new_PIDS = []
 for _, row in props.iterrows():
     datatype = row['data_type']
@@ -58,13 +76,14 @@ props.to_csv('./data/saved_properties.csv', index=False)
 print("saved properties")
 print("imported props", prop_map)
 # import items
+"""
 
 # setup wikidata integrator
-MEDIA_WIKI_API = '$MEDIA_WIKI_API'
-SPARQL_ENDPOINT = '$SPARQL_ENDPOINT'
+MEDIA_WIKI_API = 'http://localhost:8181/w/api.php'
+SPARQL_ENDPOINT = 'http://localhost:8282/proxy/wdqs/bigdata/namespace/wdq/sparql'
 
 login_instance = WI.wdi_login.WDLogin(
-    user='$BOT_USERNAME',
+    user='Pik',
     pwd='$BOT_PASSWORD',
     mediawiki_api_url=MEDIA_WIKI_API
 )
@@ -85,21 +104,22 @@ for fileIndex in range(len(item_files)):
     prop_types = list(map(lambda local_id: prop_map[local_id]['datatype'], local_prop_ids))
     PIDs = list(map(lambda local_id: prop_map[local_id]['PID'], local_prop_ids))
 
+    threads = []
 
-    new_QIDS = []
+    new_data = []
     for index, row in items.iterrows():
         label = row['label']
         if is_nan(label):
             print("Ignoreing elment with no label, ", index)
             continue
-            
+
         data = []
         for prop_index, local_prop_id in enumerate(local_prop_ids):
 
             value = row[local_prop_id]
             if is_nan(value):
                 continue
-                
+
             prop_type = prop_types[prop_index]
             PID = PIDs[prop_index]
             if prop_type == 'wikibase-item':
@@ -114,30 +134,46 @@ for fileIndex in range(len(item_files)):
             elif prop_type == 'string':
                 data.append(WI.wdi_core.WDString(value=value, prop_nr=PID))
             else:
-                print("Ignoring unknown type:", prop_type, "with value", value) 
+                print("Ignoring unknown type:", prop_type, "with value", value)
                 # add other types here
-                
+
         wd_item = WI.wdi_core.WDItemEngine(
-            data=data, 
+            data=data,
             mediawiki_api_url=MEDIA_WIKI_API,
-            sparql_endpoint_url=SPARQL_ENDPOINT
+            sparql_endpoint_url=SPARQL_ENDPOINT,
+            new_item=True,
+            core_props="P1",
         )
-        
+
         wd_item.set_label(label)
         # write to database
-        QID = wd_item.write(login_instance)
+        #QID = wd_item.write(login_instance)
         # save new QID to file and map
-        new_QIDS.append(QID)
-        local_id = row['id']
-        item_map[local_id] = QID
-        print(QID, label)
+        #new_QIDS.append(QID)
 
+        local_id = row['id']
+
+
+        def thread_function(wd_item, index, local_id, new_data):
+            global item_map
+            QID = wd_item.write(login_instance)
+            new_data.append([index, local_id, QID])
+            item_map[local_id] = QID
+            print(local_id, QID)
+        t = threading.Thread(target=thread_function,args=[wd_item, index, local_id, new_data])
+        threads.append(t)
+        t.start()
+        print("threads append: ", local_id)
+
+    while len([o for o in threads if o.is_alive()])!=0:
+        time.sleep(0.1)
+    new_data.sort(key=lambda x: x[0])
+    #for _id, _local_id, _QID in new_data:
+    #    item_map[_local_id] = _QID
+    new_data = pd.DataFrame(new_data, columns=['i', 'local_id', 'QID'])
     # after import is done, write back to new file
-    items['QID'] = pd.Series(new_QIDS, index=items.index)
+    items['QID'] = new_data['QID']
+    #items['QID'] = pd.Series(new_QIDS, index=items.index)
     items.to_csv('./data/saved_items_' + str(fileIndex) + '.csv', index=False)
     print("saved file ", './data/saved_items_' + str(fileIndex) + '.csv')
 print("import done")
-
-
-
-
